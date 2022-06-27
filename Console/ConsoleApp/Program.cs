bf4975ca9f7038c0debf971b0964a2ff1711dfe7
alamin-csedu <alaminhossain8936du@gmail.com>
2022-06-28 00:58:45 +0600

    problem : 45 minutes

--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -10,14 +10,35 @@ namespace ConsoleApp
         public static List<int> GetRank(List<int> ranked, List<int> player)
         {
             var result = new List<int>();
+            var newList = ranked.Distinct().ToList();
+            for (int i = 0; i < player.Count; i++)
+            {
+                if (newList.Contains(player[i]))
+                {
+                    result.Add(newList.IndexOf(player[i]) + 1);
+                }
+                else
+                {
+                    var lessThan = newList.TakeWhile(p => p >= player[i]).LastOrDefault();
 
-       
+                    if (player[i] < newList[newList.Count - 1])
+                    {
+                        result.Add(newList.Count + 1);
+                    }
+                    else
+                        result.Add(newList.IndexOf(lessThan) + 2);
+                }
+
+
+
+            }
+
             return result;
         }
         static void Main(string[] args)
         {
             //TODO: Change this input file path before start
-            var filePath = "D:\\Projects\\Interview\\SourceCode\\Console\\ConsoleApp\\Input.txt";
+            var filePath = "C:\\Users\\User\\Desktop\\InterviewSourceCode\\Console\\ConsoleApp\\Input.txt";
 
             var lines = System.IO.File.ReadLines(filePath).ToList();
 
